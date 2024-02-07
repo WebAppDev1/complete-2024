85ddb57b6d543d29f6036c96a04c30322ad843f1
Glitch (glitch-hello-node) <none>
2024-02-07 19:24:47 +0000

    💄🌨 Checkpoint ./public/script.js:633981/215 ./views/partials/listOneSong.hbs:633981/20

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
 console.log("Hello from the Web App Dev lab!");
 
 
+document.querySelectorAll(".delsong").forEach(element => {
+    element.addEventListener("click", (evt) => {
+      if  (!confirm('Really delete this song?')) {
+         evt.preventDefault()
+      }
+    })        
+});
