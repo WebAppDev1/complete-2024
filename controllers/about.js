d943b5c004401b36f72ab85c599c838745acd179
Glitch (glitch-hello-node) <none>
2024-03-19 09:36:40 +0000

    🎣😹 Checkpoint ./controllers/about.js:633981/2795 ./views/about.hbs:633981/2329

--- controllers/about.js
+++ controllers/about.js
@@ -1,16 +1,30 @@
-'use strict';
+"use strict";
 
 import logger from "../utils/logger.js";
+import playlistStore from "../models/playlist-store.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
+    const playlists = playlistStore.getAllPlaylists();
+
+    let numPlaylists = playlists.length;
+
+    let numSongs = 0;
+
+    for (let item of playlists) {
+      numSongs += item.songs.length;
+    }
     
+    let averagePerPlaylist = numPlaylists/numSongs;
     const viewData = {
-      title: "Playlist App About"
+      title: "Playlist App About",
+      displayNumPlaylists: numPlaylists,
+      displayNumSongs: numSongs,
+      average: averagePerPlaylist
     };
-    
-    response.render('about', viewData);
+
+    response.render("about", viewData);
   },
 };
 
